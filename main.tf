7b86fc6d3bd4b61d5a03f4f9c88e0f0f6b33677c
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-09-30 23:58:16 +0100

    Add full vpc, subnet, gateway and server

--- main.tf
+++ main.tf
@@ -4,12 +4,130 @@ provider "aws" {
     region = "us-east-1"
 }
 
-#Resource Block
-resource "aws_instance" "app_server"{
-    ami = "ami-0e86e20dae9224db8"
+# VPC
+resource "aws_vpc" "test_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+# Subnet
+resource "aws_subnet" "test_subnet" {
+  vpc_id     = aws_vpc.test_vpc.id
+  cidr_block = var.subnet_cidr
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "test_igw" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  tags = {
+    Name = var.igw_name
+  }
+}
+
+# Route Table
+resource "aws_route_table" "test_rt" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test_igw.id
+}
+}
+
+# Route Table Association
+resource "aws_route_table_association" "aws_subnet_route" {
+  subnet_id      = aws_subnet.test_subnet.id
+  route_table_id = aws_route_table.test_rt.id
+}
+
+# Security Group
+resource "aws_security_group" "test_sg" {
+  name = "test_sg"
+  vpc_id = aws_vpc.test_vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+   
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }  
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  tags = {
+        Name = "test_sg"
+    }
+}
+
+#Network Interface
+resource "aws_network_interface" "test_interface" {
+  subnet_id       = aws_subnet.test_subnet.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.test_sg.id]
+
+}
+
+#Elastic IP
+resource "aws_eip" "Test_IP" {
+  domain                    = "vpc"
+  depends_on = [aws_internet_gateway.test_igw]
+  network_interface         = aws_network_interface.test_interface.id
+  associate_with_private_ip = "10.0.1.50"
+}
+
+#EC2 Instance
+resource "aws_instance" "nginx_server"{
+    ami = var.ec2-ami
     instance_type = var.aws_instance_type
 
+    availability_zone = "us-east-1a"
+    key_name = "EC2Key"
+
+    network_interface {
+        network_interface_id = aws_network_interface.test_interface.id
+        device_index = 0
+    }
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install nginx -y
+                sudo systemctl start nginx
+                sudo bash -c 'echo your very own nginx server > /var/www/html/index.html'
+                EOF
+
     tags = {
-        Name = var.aws_instance_name
+        Name = var.ec2_instance_name
     }
 }
--- variables.tf
+++ variables.tf
@@ -9,3 +9,55 @@ variable "aws_instance_name" {
     description = "The name of the instance"
     default = "TerraformInstance"
 }
+
+variable "vpc_cidr" {
+    description = "value of the vpc cidr block"
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+    description = "value of the vpc name"
+    type = string
+    default = "TestVPC1"
+}
+
+variable "subnet_cidr" {
+    description = "value of the subnet cidr block"
+    type = string
+    default = "10.0.1.0/24"
+}
+
+variable "subnet_name" {
+    description = "value of the subnet name"
+    type = string
+    default = "TestSubnet1"
+  
+}
+
+variable "igw_name" {
+    description = "value of the internet gateway name"
+    type = string
+    default = "TestIGW1"
+}
+
+variable "ec2-ami" {
+    description = "value of the ec2 ami"
+    type = string
+    default = "ami-0e86e20dae9224db8"
+  
+}
+
+variable "ec2-instance-type" {
+    description = "value of the ec2 instance type"
+    type = string
+    default = "t2.micro"
+  
+}
+
+variable "ec2_instance_name" {
+    description = "value of the ec2 instance name"
+    type = string
+    default = "TestInstance1"
+  
+}
